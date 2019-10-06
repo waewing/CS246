4937fd3d713fb5ba3b317c39bea3eeb3fa1cb546
waewing <54601889+waewing@users.noreply.github.com>
2019-10-05 23:52:15 -0400

    Create integer.h

---
+++ integer.h
@@ -0,0 +1,22 @@
+#ifndef INTEGER_H
+#define INTEGER_H
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+namespace ds{
+
+template <class T>
+class Interger
+{
+  
+  private:
+ 
+  
+  public:
+  
+  
+};
+
+}
+#endif
