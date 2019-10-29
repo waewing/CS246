62b2668f0d9d68ee16f2916d75b0828b5cfa32e2
waewing <waewing24@gmail.com>
2019-10-29 18:10:16 -0400

    msg

--- MapMain.cpp
+++ MapMain.cpp
@@ -3,21 +3,52 @@
 #include <cctype>
 #include "linkedmap.h"
 using namespace std;
+using namespace ds;
 
-void Mode(char str[])
+char Mode(string str)
 {
-    for(int i = 0 ; i < 6; i++)
+    LinkedMap <char,int> map;
+    char ch;
+    int max = 1;
+    for(int i = 0 ; str[i] != '\0'; i++)
     {   
-        cout << tolower(str[i]);
+        ch = tolower(str[i]);
+        if(islower(ch))
+        {
+            if(map.HasKey(ch))
+            {
+                map[ch]++;
+            }
+            else
+            {
+                map.Put(ch,1);
+            }
+            if(max < map[ch])
+            {
+                max = map[ch];
+            }
+        }
     }
-    return;
+    if(map.IsEmpty())
+    {
+        return '\0';
+    }
+    for (char i = 'a' ; i<= 'z'; i+= 1)
+    {
+        if(max == map[i])
+        {
+            return i;
+        }
+    }
+
 }
+    
+
 
 int main() 
 {
-    char text[] = "Hello";
-    Mode(text);
-    cout << "\n";
+    string text = "Hello";
+    cout << Mode(text) << "\n";
 
     return 0;
 }
